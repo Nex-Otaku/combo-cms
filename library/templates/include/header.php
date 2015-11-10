fc8338a39412ae214d788c254c35068afca84805
Nex Otaku <nex@otaku.ru>
2015-11-10 10:03:14 +0300

    подключил Bootstrap 3.3.5 и jQuery 2.1.4

--- library/templates/include/header.php
+++ library/templates/include/header.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php echo htmlspecialchars($results['pageTitle']) ?></title>
-        <link rel="stylesheet" type="text/css" href="style.css" />
+        <!-- Bootstrap -->
+        <link href="css/bootstrap/bootstrap-3.3.5/css/bootstrap.min.css" rel="stylesheet">
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="js/external/jquery/jquery-2.1.4.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap/bootstrap-3.3.5/js/bootstrap.min.js"></script>
     </head>
     <body>
         <div id="container">
