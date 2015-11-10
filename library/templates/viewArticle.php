7be0e627f96e8c9fdb6b8eb24691922e76b13ecb
Nex Otaku <nex@otaku.ru>
2015-11-10 21:31:17 +0300

    вёрстка статейной страницы

--- library/templates/viewArticle.php
+++ library/templates/viewArticle.php
@@ -1,7 +1,5 @@
 <?php include TEMPLATE_PATH . "/include/header.php" ?>
- 
-      <h1 style="width: 75%;"><?php echo htmlspecialchars( $results['article']->getTitle() )?></h1>
-      <div style="width: 75%;"><?php echo $results['article']->content?></div>
 
+<?php echo $results['article']->content?>
  
 <?php include TEMPLATE_PATH . "/include/footer.php";
