074b475cb8b756aa46c51985a3249c9ad618252d
Nex Otaku <nex@otaku.ru>
2015-11-10 21:28:14 +0300

    кнопки для форм редактирования

--- library/templates/admin/editArticle.php
+++ library/templates/admin/editArticle.php
@@ -1,31 +1,40 @@
 <?php include TEMPLATE_PATH . "/include/header.php" ?>
- 
-      <form action="<?= Route::getUrl($results['formAction'], $results['formActionParams']) ?>" method="post">
-        <input type="hidden" name="articleId" value="<?php echo $results['article']->id ?>"/>
- 
-        <ul>
- 
-          <li>
+
+<form action="<?= Route::getUrl($results['formAction'], $results['formActionParams']) ?>" method="post">
+    <input type="hidden" name="articleId" value="<?php echo $results['article']->id ?>"/>
+
+    <ul>
+
+        <li>
             <label for="title">Заголовок</label>
-            <input type="text" name="title" id="title" placeholder="Название статьи" required autofocus maxlength="255" value="<?php echo htmlspecialchars( $results['article']->getTitle(false) )?>" />
-          </li>
- 
-          <li>
+            <input type="text" name="title" id="title" placeholder="Название статьи" required autofocus maxlength="255" value="<?php echo htmlspecialchars($results['article']->getTitle(false)) ?>" />
+        </li>
+
+        <li>
             <label for="content">Основной текст</label>
-            <textarea name="content" id="content" placeholder="Содержимое статьи" required maxlength="100000" style="height: 30em;"><?php echo htmlspecialchars( $results['article']->content )?></textarea>
-          </li>
- 
-        </ul>
- 
-        <div class="buttons">
-          <input type="submit" name="saveChanges" value="Сохранить" />
-          <input type="submit" formnovalidate name="cancel" value="Отмена" />
+            <textarea name="content" id="content" placeholder="Содержимое статьи" required maxlength="100000" style="height: 30em;"><?php echo htmlspecialchars($results['article']->content) ?></textarea>
+        </li>
+
+    </ul>
+
+    <div class="row">
+        <div class="col-md-3">
+            <div>
+                <button class="btn btn-success" type="submit" name="saveChanges">Сохранить</button>
+                <button class="btn btn-default" type="submit" formnovalidate name="cancel">Отмена</button>
+            </div>
         </div>
- 
-      </form>
- 
-<?php if ( $results['article']->id ) { ?>
-      <p><a href="<?= Route::getUrl('deleteArticle', array('articleId' => $results['article']->id))?>" onclick="return confirm('Вы уверены, что хотите удалить статью?')">Удалить</a></p>
-<?php } ?>
- 
-<?php include TEMPLATE_PATH . "/include/footer.php";
+        <div class="col-md-2">
+            <div>
+                <?php if ($results['article']->id) { ?>
+                    <a class="btn btn-link" href="<?= Route::getUrl('deleteArticle', array('articleId' => $results['article']->id)) ?>" onclick="return confirm('Вы уверены, что хотите удалить статью?')">Удалить</a>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+
+</form>
+
+
+<?php
+include TEMPLATE_PATH . "/include/footer.php";
--- library/templates/admin/editUser.php
+++ library/templates/admin/editUser.php
@@ -1,41 +1,49 @@
 <?php include TEMPLATE_PATH . "/include/header.php" ?>
-      
-      <?php 
-      $user = $results['user']; 
-      $newUser = empty($user->id);
-      $editCurrentUser = $user->username == User::get()->username;
-      $canEditPass = $newUser || $editCurrentUser;
-      $canDelete = !$newUser && !$editCurrentUser;
-      ?>
 
-      <form action="<?= Route::getUrl($results['formAction'], $results['formActionParams']) ?>" method="post">
-        <input type="hidden" name="userId" value="<?php echo $user->id ?>"/>
- 
-        <ul>
- 
-          <li>
+<?php
+$user = $results['user'];
+$newUser = empty($user->id);
+$editCurrentUser = $user->username == User::get()->username;
+$canEditPass = $newUser || $editCurrentUser;
+$canDelete = !$newUser && !$editCurrentUser;
+?>
+
+<form action="<?= Route::getUrl($results['formAction'], $results['formActionParams']) ?>" method="post">
+    <input type="hidden" name="userId" value="<?php echo $user->id ?>"/>
+
+    <ul>
+
+        <li>
             <label for="username">Логин</label>
-            <input type="text" name="username" id="username" placeholder="Имя пользователя" required autofocus maxlength="255" value="<?php echo htmlspecialchars( $user->username )?>" />
-          </li>
-          
-          <?php if ($canEditPass): ?>
-          <li>
-            <label for="password">Пароль</label>
-            <input type="text" name="password" id="password" placeholder="Пароль" maxlength="255" value="" />
-          </li>
-          <?php endif; ?>
- 
-        </ul>
- 
-        <div class="buttons">
-          <input type="submit" name="saveChanges" value="Сохранить" />
-          <input type="submit" formnovalidate name="cancel" value="Отмена" />
+            <input type="text" name="username" id="username" placeholder="Имя пользователя" required autofocus maxlength="255" value="<?php echo htmlspecialchars($user->username) ?>" />
+        </li>
+
+        <?php if ($canEditPass): ?>
+            <li>
+                <label for="password">Пароль</label>
+                <input type="text" name="password" id="password" placeholder="Пароль" maxlength="255" value="" />
+            </li>
+        <?php endif; ?>
+
+    </ul>
+
+    <div class="row">
+        <div class="col-md-3">
+            <div>
+                <button class="btn btn-success" type="submit" name="saveChanges">Сохранить</button>
+                <button class="btn btn-default" type="submit" formnovalidate name="cancel">Отмена</button>
+            </div>
         </div>
- 
-      </form>
- 
-<?php if ($canDelete) { ?>
-      <p><a href="<?= Route::getUrl('deleteUser', array('userId' => $user->id))?>" onclick="return confirm('Вы уверены, что хотите удалить пользователя?')">Удалить</a></p>
-<?php } ?>
- 
-<?php include TEMPLATE_PATH . "/include/footer.php";
+        <div class="col-md-2">
+            <div>
+                <?php if ($canDelete) { ?>
+                    <a class="btn btn-link" href="<?= Route::getUrl('deleteUser', array('userId' => $user->id)) ?>" onclick="return confirm('Вы уверены, что хотите удалить пользователя?')">Удалить</a>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+
+</form>
+
+<?php
+include TEMPLATE_PATH . "/include/footer.php";
