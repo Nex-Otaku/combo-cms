41e6f8b7e44929f94fb2871b0eeb91a6a19caca3
Nex Otaku <nex@otaku.ru>
2015-11-12 00:38:59 +0300

    форма входа

--- library/templates/admin/loginForm.php
+++ library/templates/admin/loginForm.php
@@ -3,23 +3,17 @@
 <form action="<?= Route::getUrl('login') ?>" method="post" style="width: 50%;">
     <input type="hidden" name="login" value="true" />
 
-    <ul>
-
-        <li>
-            <label for="username">Логин</label>
-            <input type="text" name="username" id="username" placeholder="user@server.com" required autofocus maxlength="20" />
-        </li>
-
-        <li>
-            <label for="password">Пароль</label>
-            <input type="password" name="password" id="password" placeholder="Пароль" required maxlength="20" />
-        </li>
-
-    </ul>
-
-    <div class="buttons">
-        <input type="submit" name="login" value="Вход" />
+    <div class="form-group">
+        <label for="username">Логин</label>
+        <input class="form-control" type="text" name="username" id="username" placeholder="user@server.com" required autofocus maxlength="20" />
     </div>
+
+    <div class="form-group">
+        <label for="password">Пароль</label>
+        <input class="form-control" type="password" name="password" id="password" placeholder="Пароль" required maxlength="20" />
+    </div>
+
+    <input class="btn btn-default" type="submit" name="login" value="Вход" />
 
 </form>
 
