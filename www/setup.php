992c1e3e74df128f2bea3d6828c3fe3df703bc36
Nex Otaku <nex@otaku.ru>
2015-11-11 09:17:11 +0300

    Добавил проверку на минимальную версию PHP. Должен использоваться PHP не ниже версии 5.3.6

--- www/setup.php
+++ www/setup.php
@@ -23,6 +23,14 @@ define("TEMPLATE_PATH", LIBRARY_PATH . "templates");
 define("SITE_NAME", "Combo CMS");
 define("AUTHOR_NAME", "Nex Otaku");
 
+// Минимальная версия PHP.
+define("MIN_PHP_VERSION", "5.3.6");
+
+// Проверяем версию PHP.
+if (version_compare(PHP_VERSION, MIN_PHP_VERSION) >= 0) {
+    die('Используемая версия PHP: ' . PHP_VERSION . ". Требуемая версия PHP: " . MIN_PHP_VERSION);
+}
+
 // Загружаем классы движка.
 require(CLASS_PATH . "/Article.php");
 require(CLASS_PATH . "/Db.php");
